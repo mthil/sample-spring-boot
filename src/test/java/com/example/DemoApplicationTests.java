e9bfcab711155c9297e996d26176d97073bb84ea
inow <inow@inow-formation1>
2017-09-28 14:23:35 +0200

    add all coverage methode

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -19,7 +19,7 @@ public class DemoApplicationTests {
 	
 	@Test
     public  void addition_two_integer_should_give_integer_return() throws Exception
-    {System.out.println("addition_two_integer_should_give_integer_resposne");
+    {
     if(DemoApplication.additionner(5,3)==8){
     Assert.assertTrue(true);
     }else{
@@ -29,7 +29,6 @@ public class DemoApplicationTests {
 	@Test
     public  void addition_integer_and_null_should_give_exception_return() throws Exception
     {
-		System.out.println("addition_two_integer_should_give_integer_resposne");
     try{
     	DemoApplication.additionner(5, null);
     	Assert.assertTrue(false);}
@@ -39,22 +38,63 @@ public class DemoApplicationTests {
     }
 	@Test
     public void soustraire_two_integer_should_give_integer_return() throws Exception
-    {System.out.println("after soustraire");
+    {
     if(DemoApplication.soustraire(5, 3)==2){
         Assert.assertTrue(true);
         }else{
         	Assert.assertTrue(false);
         }
     }
-//    @Test
-//    public void multiplier() throws Exception
-//    {    	System.out.println("multiplier");    }
-//    @Test
-//    public void testMultiplier2() throws Exception
-//    {System.out.println("multiplier2");}
-//    
-//    
-//    @Test
-//	public void testDiviser() throws Exception
-//    {System.out.println("ignore test diviser");   }
+	@Test
+    public  void soustraire_integer_and_null_should_give_exception_return() throws Exception
+    {
+    try{
+    	DemoApplication.soustraire(5, null);
+    	Assert.assertTrue(false);}
+    catch(Exception e){
+    	Assert.assertTrue(true);
+    }
+    }
+
+	@Test
+    public  void multiplier_two_integer_should_give_integer_return() throws Exception
+    {
+    if(DemoApplication.multiplier(5,3)==15){
+    Assert.assertTrue(true);
+    }else{
+    Assert.assertTrue(false);
+    }
+    }
+	
+	@Test
+    public  void multiplier_integer_and_null_should_give_exception_return() throws Exception
+    {
+    try{
+    	DemoApplication.multiplier(5, null);
+    	Assert.assertTrue(false);}
+    catch(Exception e){
+    	Assert.assertTrue(true);
+    }
+    }
+    
+	@Test
+    public  void diviser_two_integer_should_give_integer_return() throws Exception
+    {
+    if(DemoApplication.diviser(9,3)==3){
+    Assert.assertTrue(true);
+    }else{
+    Assert.assertTrue(false);
+    }
+    }
+	
+	@Test
+    public  void diviser_integer_and_null_should_give_exception_return() throws Exception
+    {
+    try{
+    	DemoApplication.diviser(5, null);
+    	Assert.assertTrue(false);}
+    catch(Exception e){
+    	Assert.assertTrue(true);
+    }
+    }
 }
