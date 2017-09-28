2c739e4ae153cc6a9c557c861ad55543ce1bd19f
mthil <michel.thil@hotmail.fr>
2017-09-28 11:34:41 +0200

    add methode and testCase

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -10,4 +10,32 @@ public class DemoApplication {
 		String valeur="";
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	public static Integer additionner(Integer valeurA,Integer valeurB) throws Exception{
+		if(valeurA==null||valeurB==null){
+			throw new Exception();
+		}
+		Integer result=valeurA+valeurB;
+		return result;
+	}
+	public static Integer soustraire(Integer valeurA,Integer valeurB) throws Exception{
+		if(valeurA==null||valeurB==null){
+			throw new Exception();
+		}
+		Integer result=valeurA-valeurB;
+		return result;
+	}
+	public static Integer multiplier(Integer valeurA,Integer valeurB) throws Exception{
+		if(valeurA==null||valeurB==null){
+			throw new Exception();
+		}
+		Integer result=valeurA*valeurB;
+		return result;
+	}
+	public static float diviser(Integer valeurA,Integer valeurB) throws Exception{
+		if(valeurA==null||valeurB==null){
+			throw new Exception();
+		}
+		float result=valeurA/valeurB;
+		return result;
+	}
 }
