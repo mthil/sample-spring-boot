18e7b36b4015683680e299aa96ec18aae8e8b5ac
mthil <michel.thil@hotmail.fr>
2017-09-29 11:11:54 +0200

    change testCase

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,11 +1,13 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		
+		//SpringApplication.run(DemoApplication.class, args);
 	}
 	public static Integer additionner(Integer valeurA,Integer valeurB) throws Exception{
 		if(valeurA==null||valeurB==null){
