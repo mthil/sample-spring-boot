4a8d4a763bc0acb2a84c709357d6481ebfe7b738
mthil <michel.thil@hotmail.fr>
2017-09-29 11:07:35 +0200

    add main

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,14 +1,12 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class DemoApplication {
 
-//	public static String main(String[] args) {
-//		//SpringApplication.run(DemoApplication.class, args);
-//	}
+	public static void main(String[] args) {
+		
+	}
 	public static Integer additionner(Integer valeurA,Integer valeurB) throws Exception{
 		if(valeurA==null||valeurB==null){
 			throw new Exception();
